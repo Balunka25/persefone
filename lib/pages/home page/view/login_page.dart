89dae3b6ab2538a742971bda70ef266a3951cfd9
LaisBonin <101892965+LaisBonin@users.noreply.github.com>
2022-05-24 12:05:02 -0300

    add configs

--- lib/pages/home page/view/login_page.dart
+++ lib/pages/home page/view/login_page.dart
@@ -12,57 +12,52 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.transparent,
-      body: SafeArea(
-        top: false,
-        child: Center(
-          child: Stack(
-            children: [
-              Container(
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('lib/images/background_register_page.png'),
-                    fit: BoxFit.fill,
-                  ),
-                ),
-                child: Column(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('lib/images/background_register_page.png'),
+                fit: BoxFit.fill,
+              ),
+            ),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          height: 268,
-                          width: 237,
-                          alignment: Alignment.topCenter,
-                          decoration: const BoxDecoration(
-                            image: DecorationImage(
-                              image: AssetImage('lib/images/logo.gif'),
-                              fit: BoxFit.fill,
-                            ),
-                          ),
+                    Container(
+                      height: 268,
+                      width: 237,
+                      alignment: Alignment.topCenter,
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('lib/images/logo.gif'),
+                          fit: BoxFit.fill,
                         ),
-                      ],
+                      ),
                     ),
-                    // Row(
-                    //   mainAxisAlignment: MainAxisAlignment.center,
-                    //   children: [
-                    //     Container(
-                    //       height: 300,
-                    //       width: 300,
-                    //       alignment: Alignment.center,
-                          
-                    //     ),
-                    //   ],
-                    // ),
-                    // Row(
-                    //   mainAxisAlignment: MainAxisAlignment.center,
-                      
-                    // ),
                   ],
                 ),
-              ),
-            ],
+                // Row(
+                //   mainAxisAlignment: MainAxisAlignment.center,
+                //   children: [
+                //     Container(
+                //       height: 300,
+                //       width: 300,
+                //       alignment: Alignment.center,
+                      
+                //     ),
+                //   ],
+                // ),
+                // Row(
+                //   mainAxisAlignment: MainAxisAlignment.center,
+                  
+                // ),
+              ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
