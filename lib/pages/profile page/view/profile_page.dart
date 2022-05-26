09d31bfc5eea999d12f97d6029a61552d70996dd
Isabella McGinley <isabellabitt1@hotmail.com>
2022-05-26 09:44:42 -0300

    almost done with profile page

--- lib/pages/profile page/view/profile_page.dart
+++ lib/pages/profile page/view/profile_page.dart
@@ -4,6 +4,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mobx_widget/mobx_widget.dart';
+import 'package:persefone/core/widgets/dialog_box.dart';
+import 'package:persefone/pages/login%20page/view/login_page.dart';
 import 'package:persefone/pages/profile%20page/controller/user_image_controller.dart';
 import 'package:persefone/pages/profile%20page/view/widgets/profile_page_appbar.dart';
 import '../../upload image/view/upload_image_page.dart';
@@ -36,6 +38,11 @@ class _ProfilePageState extends State<ProfilePage> {
 
   int? totalImages;
   String? userEmail;
+  var newPassword;
+  String? password;
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _newPasswordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     if (userEmail == null) {
@@ -65,7 +72,6 @@ class _ProfilePageState extends State<ProfilePage> {
                   child: Column(
                     children: [
                       Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
                         children: [
                           Container(
                             height: 80,
@@ -84,6 +90,80 @@ class _ProfilePageState extends State<ProfilePage> {
                                 .headline4!
                                 .copyWith(color: Colors.black),
                           )
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Form(
+                            child: Container(
+                              width: 180,
+                              height: 40,
+                              child: TextFormField(
+                                key: _formKey,
+                                autofocus: false,
+                                obscureText: true,
+                                decoration: InputDecoration(
+                                    suffixIcon: IconButton(
+                                      onPressed: () async {
+                                        if (_formKey.currentState!.validate()) {
+                                          setState(() {
+                                            newPassword =
+                                                _newPasswordController.text;
+                                          });
+                                          await reauthentiacateUser();
+                                          await updatePassword();
+                                          await updatePasswordAuth(newPassword);
+                                          dialogBox(
+                                            context,
+                                            "Sua senha foi atualizada!",
+                                            "Logue novamente",
+                                            "OK",
+                                            () => Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                builder: (context) =>
+                                                    const LoginPage(),
+                                              ),
+                                            ),
+                                          );
+                                        }
+                                      },
+                                      icon: const Icon(Icons.edit),
+                                      color: Colors.amber,
+                                    ),
+                                    labelText: "New Password",
+                                    hintText: "Enter New Password",
+                                    labelStyle:
+                                        Theme.of(context).textTheme.headline5,
+                                    border: const OutlineInputBorder(),
+                                    errorStyle: const TextStyle(
+                                        color: Colors.amber, fontSize: 15.0)),
+                                controller: _newPasswordController,
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return "Please enter password";
+                                  } else if (value.length < 6) {
+                                    return "Password must contain at least 6 characters";
+                                  } else if (_newPasswordController.text ==
+                                      password) {
+                                    return "Password is the same as before";
+                                  }
+                                  return null;
+                                },
+                              ),
+                            ),
+                          ),
+                          Container(
+                            height: 80,
+                            width: 80,
+                            decoration: const BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage(
+                                      "lib/images/profile_page_vaso.png"),
+                                  fit: BoxFit.cover),
+                            ),
+                          ),
                         ],
                       ),
                       Row(
@@ -198,6 +278,32 @@ class _ProfilePageState extends State<ProfilePage> {
       setState(() {
         userEmail = data['email'];
       });
+    });
+  }
+
+  Future<void> reauthentiacateUser() async {
+    await FirebaseAuth.instance
+        .signInWithEmailAndPassword(email: userEmail!, password: password!);
+    print("user reauthenticated");
+  }
+
+  Future<void> updatePassword() async {
+    final currentUser = FirebaseAuth.instance.currentUser!;
+    final firestore = FirebaseFirestore.instance;
+    if (currentUser != null) {
+      await firestore
+          .collection("users")
+          .doc(currentUser.uid)
+          .update({"password": _newPasswordController.text});
+    }
+  }
+
+  Future<void> updatePasswordAuth(String newPassword) async {
+    final user = await FirebaseAuth.instance.currentUser!;
+    user.updatePassword(newPassword).then((_) {
+      print("Successfully changed password");
+    }).catchError((error) {
+      print("Password can't be changed" + error.toString());
     });
   }
 }
