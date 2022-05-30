a8ab8362083acd2e3437b51ac5fd34e95fa93630
Isabella McGinley <isabellabitt1@hotmail.com>
2022-05-30 12:19:20 -0300

    username added to image details page

--- lib/pages/image_details_page/view/image_details_page.dart
+++ lib/pages/image_details_page/view/image_details_page.dart
@@ -48,42 +48,49 @@ class _ImageDetailsPageState extends State<ImageDetailsPage> {
 
   @override
   Widget build(BuildContext context) {
-      return Container(
-        width: double.infinity,
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage("lib/images/backgroud.png"), fit: BoxFit.cover),
-        ),
-        child: Scaffold(
-            backgroundColor: Colors.transparent,
-            body: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 80),
-                  child: Center(
-                    child: CachedNetworkImage(
-                      imageUrl: widget.imageUrl!,
-                      imageBuilder: (context, imageProvider) => Container(
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                              image: imageProvider, fit: BoxFit.cover),
-                          borderRadius: const BorderRadius.all(
-                            Radius.circular(10),
-                          ),
-                        ),
+    return Container(
+      width: double.infinity,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage("lib/images/backgroud.png"), fit: BoxFit.cover),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 80),
+              child: Center(
+                child: CachedNetworkImage(
+                  imageUrl: widget.imageUrl!,
+                  imageBuilder: (context, imageProvider) => Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: imageProvider, fit: BoxFit.cover),
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(10),
                       ),
-                      width: 300,
-                      height: 400,
-                      progressIndicatorBuilder:
-                          (context, url, downloadProgress) => Center(
-                        child: CircularProgressIndicator(
-                            value: downloadProgress.progress),
-                      ),
-                      errorWidget: (context, url, error) =>
-                          const Icon(Icons.error),
                     ),
                   ),
+                  width: 300,
+                  height: 400,
+                  progressIndicatorBuilder: (context, url, downloadProgress) =>
+                      Center(
+                    child: CircularProgressIndicator(
+                        value: downloadProgress.progress),
+                  ),
+                  errorWidget: (context, url, error) => const Icon(Icons.error),
                 ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              username,
+              style: Theme.of(context)
+                  .textTheme
+                  .headline1!
+                  .copyWith(color: MyColors.primarygreen),
+            ),
             const SizedBox(height: 30),
             ElevatedButton(
               // onPressed: () async {
