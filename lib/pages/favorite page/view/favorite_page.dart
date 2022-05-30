6fb9c0322b7196611f5ad939f3ade41c336a036d
Isabella McGinley <isabellabitt1@hotmail.com>
2022-05-30 15:11:11 -0300

    favorite page fixed

--- lib/pages/favorite page/view/favorite_page.dart
+++ lib/pages/favorite page/view/favorite_page.dart
@@ -1,13 +1,11 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:persefone/core/generics/resource.dart';
 import 'package:persefone/core/widgets/stylish_drawer.dart';
 import 'package:persefone/pages/favorite%20page/controller/favorite_controller.dart';
-import 'package:persefone/core/widgets/custom_appbar.dart';
-
 import '../../image_details_page/view/image_details_page.dart';
-
 import 'widgets/app_bar.dart';
 
 class FavoritePage extends StatefulWidget {
@@ -50,56 +48,45 @@ class _FavoritePageState extends State<FavoritePage> {
                     for (final favorite in favorites)
                       Padding(
                         padding: const EdgeInsets.all(16),
-                        // child: CachedNetworkImage(
-                        //   imageUrl: favorite['url'],
-                        //   // imageBuilder: (context, imageProvider) => Container(
-                        //   //   decoration: BoxDecoration(
-                        //   //     image: DecorationImage(
-                        //   //         image: imageProvider, fit: BoxFit.cover),
-                        //   //     borderRadius: const BorderRadius.all(
-                        //   //       Radius.circular(10),
-                        //   //     ),
-                        //   //   ),
-                        //   // ),
-                        //   progressIndicatorBuilder:
-                        //       (context, url, downloadProgress) => Center(
-                        //           child: Container(
-                        //     width: 180,
-                        //     height: 180,
-                        //     decoration: const BoxDecoration(
-                        //       image: DecorationImage(
-                        //         image:
-                        //             AssetImage("lib/images/loading_leaves.gif"),
-                        //       ),
-                        //     ),
-                        //   )),
-                        //   errorWidget: (context, url, error) =>
-                        //       const Icon(Icons.error),
-                        // ),
                         child: GestureDetector(
-                          child: Container(
-                            width: 300,
-                            height: 400,
-                            decoration: BoxDecoration(
-                              image: DecorationImage(
-                                  image: NetworkImage(favorite['url']),
-                                  fit: BoxFit.cover),
-                              borderRadius: const BorderRadius.all(
-                                Radius.circular(10),
+                          child: CachedNetworkImage(
+                            imageUrl: favorite['url'],
+                            imageBuilder: (context, imageProvider) => Container(
+                              height: 200,
+                              width: 300,
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image: imageProvider, fit: BoxFit.fitWidth),
+                                borderRadius: const BorderRadius.all(
+                                  Radius.circular(10),
+                                ),
                               ),
                             ),
+                            progressIndicatorBuilder:
+                                (context, url, downloadProgress) => Center(
+                                    child: Container(
+                              width: 180,
+                              height: 180,
+                              decoration: const BoxDecoration(
+                                image: DecorationImage(
+                                  image: AssetImage(
+                                      "lib/images/loading_leaves.gif"),
+                                ),
+                              ),
+                            )),
+                            errorWidget: (context, url, error) =>
+                                const Icon(Icons.error),
                           ),
                           onTap: () {
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: ((context) => ImageDetailsPage(
-                                              imageUrl: favorite['url'],
-                                              ownerId: favorite['owner_id']
-                                            )),
-                                      ),
-                                    );
-                                  },
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: ((context) => ImageDetailsPage(
+                                    imageUrl: favorite['url'],
+                                    ownerId: favorite['owner_id'])),
+                              ),
+                            );
+                          },
                         ),
                       )
                   ]);
